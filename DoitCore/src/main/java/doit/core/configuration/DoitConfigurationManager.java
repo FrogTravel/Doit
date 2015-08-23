89f152f229df66f9001584fcc9dfd854c8af327c
Almaz <muktaalmaz@yandex.ru>
2015-08-23 14:24:37 +0400

    Config manager added

---
+++ DoitCore/src/main/java/doit/core/configuration/DoitConfigurationManager.java
@@ -0,0 +1,10 @@
+package doit.core.configuration;
+
+/**
+ * Created by Almaz on 21.08.2015.
+ */
+public class DoitConfigurationManager {
+    public static final String DATABASE_FILE_NAME = "doit-db.zex";
+
+
+}
